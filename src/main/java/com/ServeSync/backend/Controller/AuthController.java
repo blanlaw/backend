c9f7969324e9509955110572fe9d402892bc5d6f
blanlaw <iramirezg@autonoma.edu.pe>
2024-05-18 18:30:10 -0500

    update auth

---
+++ src/main/java/com/ServeSync/backend/Controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.ServeSync.backend.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+
+
+    @PostMapping("/login")
+    public String login(){
+
+        return "login iniciado";
+
+    }
+
+    @PostMapping(value ="/register")
+
+    public String register(){
+
+        return "registro aceptado";
+
+    }
+
+
+
+}
